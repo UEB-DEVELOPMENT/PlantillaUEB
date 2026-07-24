b66978c8b25ea407f2fe1ee3486452cd3f82dac5
AlexanderA31 <ufredoalegria@hotmail.com>
2026-07-24 16:19:49 -0500

    Arreglo menu de notificaciones

--- src/components/header/NotificationDropdown.tsx
+++ src/components/header/NotificationDropdown.tsx
@@ -66,7 +66,7 @@ export default function NotificationDropdown() {
       <Dropdown
         isOpen={isOpen}
         onClose={closeDropdown}
-       className="left-1/2 -translate-x-1/2 w-[90vw] max-w-[400px] min-w-[300px] mt-[17px] flex h-[480px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark sm:left-auto sm:translate-x-0 sm:right-0 sm:w-[361px]"
+       className="!fixed !inset-x-4 mt-[17px] flex h-[480px] flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-dark sm:!inset-x-auto sm:!right-0 sm:w-[361px]"
       >
         <div className="flex items-center justify-between pb-3 mb-3 border-b border-gray-100 dark:border-gray-700">
           <h5 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
